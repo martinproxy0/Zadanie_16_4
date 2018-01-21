1a2a038c193ce9fe16c047470a2a8c94c958b05a
martinproxy0 <marcinsornek@gmail.com>
2018-01-21 15:31:14 +0100

    Add code to loaders use

---
+++ src/containers/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import uuid from 'uuid';
+import style from './css/App.css';
+import Title from '../components/Title';
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [
+                {
+                    id: 1,
+                    text: 'Some todo text 1'
+                },
+                {
+                    id: 2,
+                    text: 'Some todo text 2'
+                },
+                {
+                    id: 3,
+                    text: 'Some todo text 3'
+                }
+            ]
+        };
+    }
+    addTodo(val){
+        const todo = {
+            text: val,
+            id: uuid.v4(),
+        };
+        const data = [...this.state.data, todo];
+        this.setState({data});
+    }
+    removeTodo(id) {
+        const remainder = this.state.data.filter(todo => todo.id !== id);
+        this.setState({data: remainder});
+    }
+    render() {
+        return (
+            <div className={style.TodoApp}>
+                <Title title='abc' noumber={this.state.data.length} />                
+            </div>
+        );
+    }
+}
+
+export default App;
